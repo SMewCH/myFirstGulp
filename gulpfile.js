b14eba7988e53e8051cb755cd35902e35bb1fbf8
Suleman Shaikh <suleman.p.shaikh@gmail.com>
2018-04-22 03:00:59 -0400

    got gulp working and watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 var gulp = require('gulp');
-var exec = require('child_process');
+var exec = require('child_process').exec;
 
-gulp.watch('**/*.js', gulp.parallel());
+// gulp.watch('**/*.js', gulp.parallel());
 
 
-gulp.task('gpush', function(){
-  exec('clasp push');
+// gulp.task('gpush', function(){
+//   exec('clasp push');
+// });
+
+
+gulp.task('test', function(){
+  console.log("It is working!");
 });
+
+
+//// clasp
+gulp.task('clasp_push', function(){
+ exec('clasp push');
+});
+//
+gulp.task('watch', function(){
+ gulp.watch([
+   '**/*.gs',
+   '**/*.js',
+   '**/*.html'
+ ],
+ ['clasp_push']);
+});
+//
+gulp.task('default', ['watch']);
